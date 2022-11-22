6b3b6257d70aaccdee0a7367f811cc473ce5da05
chohyeon <chohyeonK3@gmail.com>
2022-11-22 10:58:14 +0900

    #6 강의 코딩중 - useEffect 함수 사용

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,40 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+  useEffect(() => {
+    console.log("i run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when 'counter, keyword' changes");
+  }, [counter, keyword]);
+
+  // useEffect(() => {
+  //   if (keyword !== "" && keyword.length > 5) {
+  //     console.log("search for", keyword);
+  //   }
+  // }, [keyword]); // keyword가 변화 시 코드 실행함
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input
+        type="text"
+        placeholder="Search here.."
+        onChange={onChange}
+        value={keyword}
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
