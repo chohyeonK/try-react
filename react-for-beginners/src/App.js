e43f75ec229a84c5d79a9d3d09638c673241a9b2
chohyeon <chohyeonK3@gmail.com>
2022-11-22 11:12:53 +0900

    #6 강의 코딩 - cleanup 사용

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,40 +1,24 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
+function Hello() {
   useEffect(() => {
-    console.log("i run only once.");
+    console.log("hi :)");
+    return () => {
+      console.log("bye :(");
+    };
   }, []);
-  useEffect(() => {
-    console.log("i run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("i run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("i run when 'counter, keyword' changes");
-  }, [counter, keyword]);
+  return <h1>Hello</h1>;
+}
 
-  // useEffect(() => {
-  //   if (keyword !== "" && keyword.length > 5) {
-  //     console.log("search for", keyword);
-  //   }
-  // }, [keyword]); // keyword가 변화 시 코드 실행함
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Search here.."
-        onChange={onChange}
-        value={keyword}
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
